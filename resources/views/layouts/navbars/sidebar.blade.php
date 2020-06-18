eb5fc801438a245e54ab11c3173466abdccae733
Jorge Eduardo <corpjorge@hotmail.com>
2020-06-18 00:26:48 -0500

    update

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -55,7 +55,7 @@
           <ul class="nav">
             <li class="nav-item{{ $activePage == 'profile' ? ' active' : '' }}">
               <a class="nav-link" href="{{ route('profile.edit') }}">
-                <span class="sidebar-mini"> UP </span>
+                <span class="sidebar-mini"> P </span>
                 <span class="sidebar-normal">{{ __('Perfil') }} </span>
               </a>
             </li>
@@ -63,7 +63,7 @@
             @can('manage-users', App\User::class)
               <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
                   <a class="nav-link" href="{{ route('user.index') }}">
-                      <span class="sidebar-mini"> UM </span>
+                      <span class="sidebar-mini"> A </span>
                       <span class="sidebar-normal"> {{ __('Asociados') }} </span>
                   </a>
               </li>
@@ -81,7 +81,7 @@
             @can('manage-roles', App\Role::class)
               <li class="nav-item{{ $activePage == 'role-management' ? ' active' : '' }}">
                 <a class="nav-link" href="{{ route('role.index') }}">
-                  <span class="sidebar-mini"> RM </span>
+                  <span class="sidebar-mini"> R </span>
                   <span class="sidebar-normal"> {{ __('Roles') }} </span>
                 </a>
               </li>
