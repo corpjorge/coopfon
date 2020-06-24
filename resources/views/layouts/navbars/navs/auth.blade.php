7b919926eaf79b9d920d6b50c7cad651f9d47be6
Jorge Eduardo <corpjorge@hotmail.com>
2020-06-24 01:17:08 -0500

    conf auth

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -49,8 +49,7 @@
               </p>
             </a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
-                <a class="dropdown-item" href="{{ route('profile.edit') }}">{{ __('Perfil') }}</a>
-                <a class="dropdown-item" href="#">{{ __('Configuración') }}</a>
+                <a class="dropdown-item" href="{{ route('profile.edit') }}">{{ __('Perfil') }}</a>                
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">{{ __('Cerrar sesión') }}</a>
             </div>
