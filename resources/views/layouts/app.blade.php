66b6e44973d1db13f4ee2d50c3eda7bb93844af7
Jorge Eduardo <corpjorge@hotmail.com>
2020-06-30 22:58:51 -0500

    eliminar comentario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,6 @@ Página del producto: https://www.coopfon.com/
         <script src="{{ asset('coopfon') }}/js/material-dashboard.js?v=2.1.0" type="text/javascript"></script>
 
         <script src="{{ asset('coopfon') }}/js/application.js"></script>
-{{--        <script src="{{ asset('coopfon') }}/js/sidebar.js"></script>--}}
 
         <script>
           $(document).ready(function () {
